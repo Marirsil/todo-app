c823645868bf47ef7c956cfbaa7c17f342eca31a
Mariana Silva <marir24k@gmail.com>
2023-11-30 01:23:17 -0300

    exibição das tarefas na página home concluida

--- index.js
+++ index.js
@@ -50,11 +50,9 @@ app.get('/', (requisicao, resposta) => {
             }
         })
 
-
-        
+        resposta.render('home', { tarefas })
     })
 
-    resposta.render('home')
 })
 
 const conexao = mysql.createConnection({
